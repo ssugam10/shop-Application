bbd9c118b7cde0896885ba9a80766b16e94c1ec9
sugam <ssugamlm10@gmail.com>
2024-05-31 18:22:56 +0300

    Adding session data to views in general way and fixing postorder controller bug

--- controllers/shop.js
+++ controllers/shop.js
@@ -37,7 +37,6 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: "Shop",
         path: "/",
-        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => {
@@ -93,7 +92,7 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user._id,
         },
         products: products,
