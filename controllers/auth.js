5e1405918f6bea28d3aa7823a89b2e9fc25e9b67
sugam <ssugamlm10@gmail.com>
2024-06-04 19:20:27 +0300

    Adding validation for email in signup page using express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,8 @@ const bcrypt = require("bcryptjs");
 //const Mailjet = require("node-mailjet");
 const nodemailer = require("nodemailer");
 
+const {validationResult} = require('express-validator');
+
 require("dotenv").config();
 
 // const mailjet = Mailjet.apiConnect(
@@ -107,6 +109,17 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()){
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
 
   User.findOne({ email: email })
     .then((userDoc) => {
